f9d9d2e2df553a3f7157d6fd1858e406a5818b9c
hansenjl <jenn.leigh.hansen@gmail.com>
2021-02-03 22:48:47 -0800

    finish solution

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -11,4 +11,36 @@ class ItemsController < ApplicationController
         item = Item.find(params[:id])
         render json: item.to_json(except: [:created_at, :updated_at], include: {category: {only: [:name]}})
     end
+
+    def create 
+        item = Item.new(item_params)
+        item.category = Category.first
+        if item.save 
+            render json: ItemSerializer.new(item)
+        else
+            render json: {error: "could not make item"}
+        end
+    end
+
+    def update 
+        item = Item.find(params[:id])
+        item.update(item_params)
+        if item.save 
+            render json: ItemSerializer.new(item)
+        else
+            render json: {error: "could not update item"}
+        end
+    end
+
+    def destroy 
+        item = Item.find(params[:id])
+        item.destroy 
+        render json: {message: "successfully deleted #{item.name}!"}
+    end
+
+    private 
+
+    def item_params
+        params.require(:item).permit(:name, :description, :price)
+    end
 end
--- frontend/index.js
+++ frontend/index.js
@@ -1,25 +1,145 @@
 // make a fetch request to the '/items' and display items on the DOM
 const list = document.getElementById('item-list')
+const form = document.getElementById('item-form')
+const priceInput = document.getElementById('item-price')
+const nameInput = document.getElementById('item-name')
+const descriptionInput = document.getElementById('item-description')
 
-fetch('http://localhost:3000/items')
+form.addEventListener('submit', handleFormSubmit)
+
+function handleFormSubmit(e){
+    e.preventDefault()
+    const formData = {
+        name: nameInput.value, 
+        price: priceInput.value, 
+        description: descriptionInput.value, 
+    }
+
+    const configObj = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+
+    fetch('http://localhost:3000/items', configObj)
+        .then(r => r.json())
+        .then(data => {
+            addItemToDOM(data.data)
+            form.reset()
+        })
+}
+
+function fetchItems(){
+    fetch('http://localhost:3000/items')
     // .then(function(r){
     //     return r.json()
     // })   // this is the same as the .then on line 8
     .then(r => r.json())
     .then(renderItems)
-
+}
 
 function renderItems(arg){
     const items = arg["data"]
-    const liElements = items.map(function(item){
-        const li = document.createElement('li')
-        li.innerText = `${item.attributes.name} - $${item.attributes.price}`
-        return li
-    })
-    // debugger
-    liElements.forEach(element => {
-        list.appendChild(element)
+    items.forEach(element => {
+        addItemToDOM(element)
     });
-   
+}
+
+function addItemToDOM(item){
+    const li = document.createElement('li')
+    li.id = `item-${item.id}`
+    render(li, item.attributes)
+    list.appendChild(li)
+    li.addEventListener('click', handleItemClick)
+}
+
+function render(li, data){
+
+    li.innerHTML = `
+        <div>
+        $<span class="price">${data.price}</span> <strong class="item-name">${data.name}</strong>: 
+        <span class="description">${data.description}</span><br>
+        </div>
+        <button class="edit">Edit</button>
+        <button class="delete">Delete</button>
+    `
+}
+
+function handleItemClick(e){
+    if(e.target.innerText === "Edit"){
+        renderEditForm(e.target.parentElement)
+        e.target.innerText = "Save"
+    }else if(e.target.innerText === "Delete"){
+        deleteElement(e.target.parentElement)
+    }else if(e.target.innerText === "Save"){
+        patchElement(e.target.parentElement)
+        e.target.innerText = "Edit"
+    }
+}
+
+function renderEditForm(liElement){
+    const name = liElement.querySelector('.item-name').innerText
+    const description = liElement.querySelector('.description').innerText
+    const price = liElement.querySelector('.price').innerText
+    liElement.querySelector('div').innerHTML = `
+    <input type="text" name="name" id="update-name" value="${name}">
+    <input type="text" name="description" id="update-description" value="${description}">
+    <input type="number" name="price" id="update-price" min="0" step=".01" value="${price}">
+    `
+}
+
+function patchElement(liElement){
+    const name = liElement.querySelector('#update-name').value
+    const description = liElement.querySelector('#update-description').value
+    const price = liElement.querySelector('#update-price').value
+  
+    const formData = {
+        name: name, 
+        price: price, 
+        description: description, 
+    }
+
+    const configObj = {
+        method: 'PATCH',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify(formData)
+    }
+
+    const id = liElement.id.split("-")[1]
+    fetch(`http://localhost:3000/items/${id}`, configObj)
+        .then(r => r.json())
+        .then(data => {
+            render(liElement, data.data.attributes)
+            form.reset()
+        })
+}
+
+function deleteElement(liTag){
+    liTag.remove()
+    const id = liTag.id.split("-")[1]
+
+    const configObj = {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+    }
+
+    fetch(`http://localhost:3000/items/${id}`, configObj)
+        .then(r => r.json())
+        .then(data => {
+            console.log(data)
+            alert(data.message)
+        })
 
 }
+
+
+fetchItems()
