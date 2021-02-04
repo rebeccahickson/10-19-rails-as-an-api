5dfe113643dce4b3994b84a72ee6389a577ac701
hansenjl <jenn.leigh.hansen@gmail.com>
2021-02-04 08:59:19 -0800

    study group pt 2

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -11,4 +11,26 @@ class ItemsController < ApplicationController
         item = Item.find(params[:id])
         render json: item.to_json(except: [:created_at, :updated_at], include: {category: {only: [:name]}})
     end
+
+    def create 
+        item = Item.new(item_params)
+        item.category = Category.last  ## NEEDS TO CHANGE!!!!
+        if item.save 
+            render json: ItemSerializer.new(item)
+        else
+            render json: {error: "could not save"}
+        end
+    end
+
+    def destroy 
+        item = Item.find(params[:id])
+        item.destroy 
+        render json: {message: "successfully deleted #{item.name}"}
+    end
+
+    private 
+
+    def item_params
+        params.require(:item).permit(:price, :description, :name)
+    end
 end
--- frontend/index.js
+++ frontend/index.js
@@ -5,13 +5,43 @@ const priceInput = document.getElementById('item-price')
 const nameInput = document.getElementById('item-name')
 const descriptionInput = document.getElementById('item-description')
 
+form.addEventListener('submit', handleSubmit)
 
-fetch('http://localhost:3000/items')
+function handleSubmit(e){
+   e.preventDefault()
+
+   //make our params hash
+   const itemInfo = {
+        price:  priceInput.value,
+        name: nameInput.value,
+        description: descriptionInput.value
+   }
+
+   const configObj = {
+       method: 'POST',
+       headers: {
+           "Content-Type": "application/json",
+           Accept: "application/json"
+       },
+       body: JSON.stringify(itemInfo)
+   }
+  
+   // pessimistic rendering 
+   fetch("http://localhost:3000/items", configObj)
+    .then(r => r.json())
+    .then(json => renderItem(json.data))
+}
+
+function getItems(){
+    fetch('http://localhost:3000/items')
     // .then(function(r){
     //     return r.json()
     // })   // this is the same as the .then on line 8
     .then(r => r.json())
     .then(renderItems)
+
+}
+
 
 
 function renderItems(arg){
@@ -23,12 +53,56 @@ function renderItems(arg){
 
 function renderItem(item){
     const li = document.createElement('li')
+    li.dataset["id"] = item.id
+    li.id = `item-${item.id}`
     li.innerHTML = `
-        <div>
+        <div data-id="${item.id}">
             $<span class="price">${item.attributes.price}</span>
             <strong class="name">${item.attributes.name}</strong>:
             <span class="description">${item.attributes.description}</span> 
-        </div
+        </div>
+        <button class="edit" data-id="${item.id}">Edit</button>
+        <button class="delete" data-id="${item.id}">Delete</button>
     `
     list.appendChild(li)
+    li.addEventListener('click', handleLiClick)
+    
+    // const deleteBtn = li.querySelector('.delete')
+    // deleteBtn.addEventListener('click', deleteItem)
+
 }
+
+function handleLiClick(e){
+    if(e.target.innerText === "Edit"){
+
+    } else if (e.target.innerText === "Delete"){
+        deleteItem(e)
+    }
+}
+
+//optomistic rendering
+function deleteItem(e){
+    e.target.parentElement.remove() // remove it before the fetch request 
+    const id = e.target.dataset.id 
+
+    const configObj = {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        }
+    }
+    
+    fetch(`http://localhost:3000/items/${id}`, configObj)
+        .then(r => r.json())
+        .then(json => alert(json.message))
+
+}
+
+// add event listerner 
+// submit a fetch request to delete 
+// .remove() it from the DOM
+
+
+
+getItems()
